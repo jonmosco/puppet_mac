121cc1071fda349d9597f31abdb1ba80f293afb5
Jon Mosco <jonny.mosco@gmail.com>
2013-06-28 16:19:45 -0400

    added command to enable hidden folders.  Needs work

--- prefs.pp
+++ prefs.pp
@@ -5,3 +5,8 @@ exec { 'Unhide ~/Library':
   command => "/usr/bin/chflags nohidden ${home}/Library",
   onlyif  => "/bin/ls -Ol ${home} | grep Library | grep Hidden";
 }
+
+exec { 'HiddenFiles':
+  command => 'defaults write com.Apple.Finder AppleShowAllFiles TRUE',
+  onlyif  => 'Not sure what to put here yet';
+}
